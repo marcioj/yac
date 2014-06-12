ada58b286e57e09622e9c1847c01bbfc9aee4ed8
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-06-12 20:38:18 -0300

    Created extend and init

--- test/class_test.js
+++ test/class_test.js
@@ -7,4 +7,70 @@ describe("Class", function() {
   it("exists", function() {
     expect(Class).to.exist;
   });
+
+  it(".extend generate a new instance", function() {
+    var Sub = Class.extend({
+      foo: "bar",
+      hello: function() {
+        return "world";
+      }
+    });
+    var sub = new Sub();
+    expect(sub.foo).to.equal("bar");
+    expect(sub.hello()).to.equal("world");
+  });
+
+  it("change properties without affect other instances", function() {
+    var Sub = Class.extend({
+      foo: "bar",
+      hello: function() {
+        return "world";
+      }
+    });
+    var sub = new Sub();
+    expect(sub.foo).to.equal("bar");
+
+    sub.foo = "my bar";
+    var otherSub = new Sub();
+
+    expect(otherSub.foo).to.equal("bar");
+    expect(sub.foo).to.equal("my bar");
+  });
+
+  it("change properties without affect other instances", function() {
+    var Sub = Class.extend({
+      foo: "bar",
+      hello: function() {
+        return "world";
+      }
+    });
+    var sub = new Sub();
+    expect(sub.foo).to.equal("bar");
+
+    sub.foo = "my bar";
+    var otherSub = new Sub();
+
+    expect(otherSub.foo).to.equal("bar");
+    expect(sub.foo).to.equal("my bar");
+  });
+
+  it("call init when a new instance is created", function() {
+    var args;
+    var times = 0;
+    var Sub = Class.extend({
+      init: function() {
+        times++;
+        args = Array.prototype.slice.call(arguments);
+      }
+    });
+    new Sub();
+
+    expect(times).to.equal(1);
+    expect(args).to.deep.equal([]);
+
+    new Sub(1, 2, 3);
+
+    expect(times).to.equal(2);
+    expect(args).to.deep.equal([1, 2, 3]);
+  });
 });
