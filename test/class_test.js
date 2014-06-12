7398450f27acdbbece7d3d5b49e22d4b1a701b54
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-06-12 20:09:08 -0300

    Initial setup

---
+++ test/class_test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Class = require("../lib/class");
+var expect = require("chai").expect;
+
+describe("Class", function() {
+  it("exists", function() {
+    expect(Class).to.exist;
+  });
+});
