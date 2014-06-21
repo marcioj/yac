ee2d2b54b508bb8924214de33532fb39ae5cf23c
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-06-21 19:49:59 -0300

    Added broccoli

---
+++ Brocfile.js
@@ -0,0 +1,52 @@
+var pickFiles = require("broccoli-static-compiler");
+var browserify = require("broccoli-browserify");
+var mergeTrees = require("broccoli-merge-trees");
+var env = require("broccoli-env").getEnv();
+
+var dev = env === "development";
+
+if (dev) {
+  var sourceCodeTree = pickFiles("lib", {
+    srcDir: "/",
+    destDir: "/lib"
+  });
+
+  var testTree = pickFiles("test", {
+    srcDir: "/",
+    destDir: "/test"
+  });
+
+  var testIndexTree = pickFiles("test", {
+    srcDir: "/",
+    files: ["test.html"],
+    destDir: "/"
+  });
+
+  var mochaTree = pickFiles("node_modules", {
+    srcDir: "/mocha",
+    files: ["mocha.js", "mocha.css"],
+    destDir: "/"
+  });
+
+  var appTree = mergeTrees([sourceCodeTree, testTree]);
+
+  var appTree = browserify(appTree, {
+    entries: ["./test/class_test.js", "./lib/class"],
+    outputFile: "bundle.js"
+  });
+
+  module.exports = mergeTrees([appTree, testIndexTree, mochaTree]);
+} else {
+  var sourceCodeTree = pickFiles("lib", {
+    srcDir: "/",
+    destDir: "/lib"
+  });
+
+  var appTree = browserify(sourceCodeTree, {
+    entries: ["./lib/class"],
+    bundle: { standalone: "Class" },
+    outputFile: "class.js"
+  });
+
+  module.exports = appTree;
+}
--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 
 TODO
 
+# Develpoment
+
 ## Roadmap
 
 - [x] Classical class inheritance via Class.extend
@@ -16,3 +18,13 @@ TODO
 - [ ] Class Mixins
 - [ ] More tests
 - [ ] Use in a production project
+
+## Running tests
+
+Run `npm start` and go to http://localhost:4200/test.html to run the tests in the browser
+Use `npm test` to run the tests in the console
+
+## Building from source
+
+Git clone the repo and run `npm run-script build` to build a production source in the `dist` folder
+
