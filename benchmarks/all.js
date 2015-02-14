30a3d996b2ce2efd361eb6d9bf48d58620f65161
Marcio Junior <marciojunior_eu@yahoo.com.br>
2015-02-14 20:01:00 -0200

    Make Sub.overrideClass optimized

--- benchmarks/all.js
+++ benchmarks/all.js
@@ -12,18 +12,7 @@ function printStatus(fn, fnName) {
   }
 }
 
-// Taken from https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#1-tooling
-// Fill type-info
-Class.extend();
-// 2 calls are needed to go from uninitialized -> pre-monomorphic -> monomorphic
-Class.extend();
-
-%OptimizeFunctionOnNextCall(Class.extend);
-// The next call
-Class.extend();
-
-// Check
-printStatus(Class.extend, "Class.extend");
+console.log('================ Benchmarks ================');
 
 Suite
   .add('Class.extend no args', function () {
@@ -38,7 +27,40 @@ Suite
     var Other = { bar: "bar" };
     Class.extend(Base, Other);
   })
+  .add('Sub.overrideClass no args', function () {
+    var Sub = Class.extend();
+    Sub.overrideClass();
+  })
+  .add('Sub.overrideClass one arg', function () {
+    var Sub = Class.extend();
+    var Base = { foo: "foo" };
+    Sub.overrideClass(Base);
+  })
+  .add('Sub.overrideClass two args', function () {
+    var Sub = Class.extend();
+    var Base = { foo: "foo" };
+    var Other = { bar: "bar" };
+    Sub.overrideClass(Base, Other);
+  })
   .on('cycle', function(event) {
     console.log(String(event.target));
   })
   .run();
+
+console.log('================ Optimizations ================');
+// Taken from https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#1-tooling
+// Class.extend
+Class.extend(); // Fill type-info
+Class.extend(); // 2 calls are needed to go from uninitialized -> pre-monomorphic -> monomorphic
+%OptimizeFunctionOnNextCall(Class.extend);
+Class.extend(); // The next call
+printStatus(Class.extend, "Class.extend"); // Check
+
+// Sub.overrideClass
+var Sub = Class.extend();
+Sub.overrideClass(); // Fill type-info
+Sub.overrideClass(); // 2 calls are needed to go from uninitialized -> pre-monomorphic -> monomorphic
+%OptimizeFunctionOnNextCall(Sub.overrideClass);
+Sub.overrideClass(); // The next call
+printStatus(Sub.overrideClass, "Sub.overrideClass"); // Check
+
